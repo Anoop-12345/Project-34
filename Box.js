4a7cd3cb87f8ff9395ea6e5d12d5230bf83569a8
Anoop-12345 <78742092+Anoop-12345@users.noreply.github.com>
2021-11-12 21:24:31 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -3,7 +3,13 @@ class Box {
     var options = {
         'restitution':0.8,
         'friction':1.0,
-        'density':0.04,
+        'density':20,
+
+        // 'isStatic': true
+        // 'isStatic'= false
+        // 'isStatic'= true
+         'isStatic': false
+        
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
@@ -19,8 +25,8 @@ class Box {
     rotate(angle);
     rectMode(CENTER);
     strokeWeight(4);
-    stroke("green");
-    fill(255);
+    stroke("black");
+    fill("red");
     rect(0, 0, this.width, this.height);
     pop();
   }
